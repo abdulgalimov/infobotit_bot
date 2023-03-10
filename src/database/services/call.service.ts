d615f73c1a01c936d4c209b8cb3afce53ff32446
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2023-03-10 22:29:41 +0300

    auto delete finished calls

--- src/database/services/call.service.ts
+++ src/database/services/call.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, LessThan } from 'typeorm';
 import { CallEntity } from '../entities/call.entity';
 import { ICall } from '../../types/interfaces/call';
 
@@ -9,7 +9,20 @@ export class CallService {
   constructor(
     @InjectRepository(CallEntity)
     private callRepository: Repository<CallEntity>,
-  ) {}
+  ) {
+    setInterval(() => {
+      this.clearFinishedCalls();
+    }, 1000);
+  }
+
+  async clearFinishedCalls() {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+
+    await this.callRepository.delete({
+      finishedAt: LessThan(date),
+    });
+  }
 
   async findById(callId: string): Promise<CallEntity> {
     return this.callRepository.findOne({
